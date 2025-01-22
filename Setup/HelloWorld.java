7179610a141cdc3a00b272be1b75c41ba339caa3
ErgimanGU <ergisenja@hotmail.com>
2025-01-22 17:10:32 +0100

    Fix file name

---
+++ Setup/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld extends Thread {
+    @Override
+    public void run() {
+      System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+      HelloWorld thread = new HelloWorld();
+      thread.start();
+    }
+  }
