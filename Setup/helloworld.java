0abda9856b2a0632f3a1cb939a0babd544efeca0
ErgimanGU <ergisenja@hotmail.com>
2025-01-22 17:04:47 +0100

    Auto stash before merge of "main" and "origin/main"

--- Setup/helloworld.java
+++ Setup/helloworld.java
@@ -1,19 +1,11 @@
-package Setup;
-
-class CPrintHelloworld implements Runnable
-{
-    public void run()
-        {
-            System.out.println("hello world");
-        }
-}
-
-class HelloWorld
-{
-  public static void main(String args[])
-  {
-    CPrintHelloworld a = new CPrintHelloworld();
-    Thread t = new Thread(a);
-    t.start();
+public class HelloWorld extends Thread {
+    @Override
+    public void run() {
+      System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+      HelloWorld thread = new HelloWorld();
+      thread.start();
+    }
   }
-}
+  
